77478ce1b19c1ffffb520e3d8e5ea8f3f39ac4b7
Danai Nanou <danainanou@Danais-MacBook-Air.local>
2017-03-08 01:20:46 +0200

    done n dusted

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,9 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
 // if ((cardTwo === cardFour) || (cardOne === cardThree)) {
 // 	alert("Sorry, not a match.")
@@ -15,13 +15,67 @@ var cardFour = "king";
 // 	alert("You found a match!")
 // }
 
-var gameBoard = document.getElementById('game-board');
+// var gameBoard = document.getElementById('game-board');
 
-var createCards = function() {
-	for (var i = 0; i < 4; i++) {
-		var newCards = document.createElement('div');
-			newCards.className = 'card';
-			gameBoard.appendChild(newCards);
-		}
+// var createCards = function() {
+// 	for (var i = 0; i < 4; i++) {
+// 		var newCards = document.createElement('div');
+// 			newCards.className = 'card';
+// 			gameBoard.appendChild(newCards);
+// 		}
+// }
+// createCards()
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var gameBoard = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var newCards = document.createElement('div');
+	    newCards.className = 'card';
+	    newCards.setAttribute('data-card', cards[i]);
+	    newCards.addEventListener('click', isTwoCards);
+	    gameBoard.appendChild(newCards);
+  }
+
 }
-createCards()
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='images/kingspades.png'>";
+	} else {
+		this.innerHTML = "<img src='images/queenspade.png'>";
+	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("It's a match, woo!");
+  } else {
+    alert("Aww, give it another go.");
+
+  }
+}
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+

45d18c95a084a2b5ad530a60e86a0874509fb388
Danai Nanou <danainanou@Danais-MacBook-Air.local>
2017-03-07 10:25:26 +0200

    added js cards

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -4,3 +4,24 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// if ((cardTwo === cardFour) || (cardOne === cardThree)) {
+// 	alert("Sorry, not a match.")
+// }
+// else if ((cardOne === cardFour) || (cardTwo == cardFour)) {
+// 	alert("Sorry, not a match.")
+// }
+// else {
+// 	alert("You found a match!")
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i = 0; i < 4; i++) {
+		var newCards = document.createElement('div');
+			newCards.className = 'card';
+			gameBoard.appendChild(newCards);
+		}
+}
+createCards()
